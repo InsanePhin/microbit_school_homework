f9d7e3a63016f9f7013f9338a713ab354fa6cbbb
Minseo Seo <insanephin@gmail.com>
2021-09-30 09:47:21 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.py
@@ -0,0 +1,26 @@
+basic.show_leds("""
+    . # . # .
+        . # . # .
+        . . # . .
+        . # . # .
+        . # . # .
+""")
+music.play_melody("C - - - - - - - ", music.beat(BeatFraction.WHOLE))
+music.play_melody("C - - - - - - - ", 180)
+music.play_melody("C - - - - - - - ", 180)
+music.play_melody("C - - - - - - - ", 120)
+music.play_melody("C - - - - - - - ", 120)
+music.play_melody("E - - - - - - - ", 120)
+music.play_melody("G - - - - - - - ", 180)
+music.play_melody("G - - - - - - - ", 180)
+music.play_melody("E - - - - - - - ", 120)
+music.play_melody("C - - - - - - - ", 120)
+music.play_melody("G - - - - - - - ", 180)
+music.play_melody("G - - - - - - - ", 180)
+music.play_melody("E - - - - - - - ", 120)
+music.play_melody("G - - - - - - - ", 180)
+music.play_melody("G - - - - - - - ", 180)
+music.play_melody("E - - - - - - - ", 120)
+music.play_melody("C - - - - - - - ", 120)
+music.play_melody("C - - - - - - - ", 120)
+music.play_melody("C - - - - - - - ", 120)
---
+++ main.ts
@@ -0,0 +1,26 @@
+basic.showLeds(`
+    . # . # .
+        . # . # .
+        . . # . .
+        . # . # .
+        . # . # .
+`)
+music.playMelody("C - - - - - - - ", music.beat(BeatFraction.Whole))
+music.playMelody("C - - - - - - - ", 180)
+music.playMelody("C - - - - - - - ", 180)
+music.playMelody("C - - - - - - - ", 120)
+music.playMelody("C - - - - - - - ", 120)
+music.playMelody("E - - - - - - - ", 120)
+music.playMelody("G - - - - - - - ", 180)
+music.playMelody("G - - - - - - - ", 180)
+music.playMelody("E - - - - - - - ", 120)
+music.playMelody("C - - - - - - - ", 120)
+music.playMelody("G - - - - - - - ", 180)
+music.playMelody("G - - - - - - - ", 180)
+music.playMelody("E - - - - - - - ", 120)
+music.playMelody("G - - - - - - - ", 180)
+music.playMelody("G - - - - - - - ", 180)
+music.playMelody("E - - - - - - - ", 120)
+music.playMelody("C - - - - - - - ", 120)
+music.playMelody("C - - - - - - - ", 120)
+music.playMelody("C - - - - - - - ", 120)
